d56a7c7efff144856c3a6a122ab8589dab592a32
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2021-04-21 22:59:04 +0530

    adding background

--- core/src/com/singh/nilesh/Supermario.java
+++ core/src/com/singh/nilesh/Supermario.java
@@ -8,26 +8,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Supermario extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		/*background.dispose();*/
 	}
 }
