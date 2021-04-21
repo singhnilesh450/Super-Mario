f130a7d2b6511c33f4dcc055938d1d0f8fe96032
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2021-04-22 00:37:06 +0530

    add coin and make then move

--- core/src/com/singh/nilesh/Supermario.java
+++ core/src/com/singh/nilesh/Supermario.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Supermario extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
@@ -14,7 +17,14 @@ public class Supermario extends ApplicationAdapter {
 	int gameState=0;
 	float velocity;
 	float manY;
-	int gravity=2;
+	float gravity = 0.2f;
+	int pause=0;
+	Random random;
+
+	ArrayList<Integer> coinX=new ArrayList<>();
+	ArrayList<Integer> coinY=new ArrayList<>();
+	int coincount=0;
+	Texture coin;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -26,30 +36,55 @@ public class Supermario extends ApplicationAdapter {
 		man[3]=new Texture("frame-4.png");
 		manY=Gdx.graphics.getHeight()/2-man[0].getHeight()/2;
 
+		coin=new Texture("coin.png");
+		random=new Random();
 	}
 
+	public  void  makeCoin()
+	{
+     float height=  random.nextFloat() *Gdx.graphics.getHeight();
+     coinY.add((int)height);
+     coinX.add(Gdx.graphics.getWidth());
+	}
 	@Override
 	public void render () {
 
 		batch.begin();
 		batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
-		if(manstate<3)
-			manstate++;
-		else
-			manstate=0;
+		if(coincount<100){
+			coincount++;
+			/*Gdx.app.log("sdsdf",String.valueOf(coincount));*/
+		}else{
+			coincount=0;
+			makeCoin();
+			/*Gdx.app.log("sdsdf","haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");*/
+		}
+		for(int i=0;i<coinX.size();i++){
+			batch.draw(coin,coinX.get(i),coinY.get(i));
+			coinX.set(i,coinX.get(i)-4);
+		}
+        if(pause<8)
+        	pause++;
+        else {
+			pause = 0;
+			if (manstate < 3)
+				manstate++;
+			else
+				manstate = 0;
+		}
 		if(gameState==0) {
 			if (Gdx.input.justTouched())
 				gameState = 1;
 		}
 		else if(gameState==1){
 			if (Gdx.input.justTouched())
-			velocity-=25;
+			velocity=-10;
 
-
-			if(manY>0 ) {
-				velocity = velocity + gravity;
-				manY -= velocity;
+			velocity = velocity + gravity;
+			manY -= velocity;
+			if(manY<=0 ) {
+				manY=0;
 			}/*else{
 			gameState=2;
 		}*/
