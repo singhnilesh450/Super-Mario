2e65f126bbdb9e36ae9c1eed4bf0fb014d9cfc79
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2021-04-21 23:30:39 +0530

    make man move vertically

--- core/src/com/singh/nilesh/Supermario.java
+++ core/src/com/singh/nilesh/Supermario.java
@@ -9,11 +9,23 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Supermario extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
-	
+	Texture[] man;
+	int manstate=0;
+	int gameState=0;
+	float velocity;
+	float manY;
+	int gravity=2;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+		man=new Texture[4];
+		man[0]=new Texture("frame-1.png");
+		man[1]=new Texture("frame-2.png");
+		man[2]=new Texture("frame-3.png");
+		man[3]=new Texture("frame-4.png");
+		manY=Gdx.graphics.getHeight()/2-man[0].getHeight()/2;
+
 	}
 
 	@Override
@@ -21,6 +33,30 @@ public class Supermario extends ApplicationAdapter {
 
 		batch.begin();
 		batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+		if(manstate<3)
+			manstate++;
+		else
+			manstate=0;
+		if(gameState==0) {
+			if (Gdx.input.justTouched())
+				gameState = 1;
+		}
+		else if(gameState==1){
+			if (Gdx.input.justTouched())
+			velocity-=25;
+
+
+			if(manY>0 ) {
+				velocity = velocity + gravity;
+				manY -= velocity;
+			}/*else{
+			gameState=2;
+		}*/
+		}
+
+		batch.draw(man[manstate], Gdx.graphics.getWidth()/2-man[manstate].getWidth()/2,manY);
+
 		batch.end();
 	}
 	
