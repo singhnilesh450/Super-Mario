cd1bf526846db3c11da3f0ecf24ae03ab0a6e032
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2021-04-21 22:47:55 +0530

    craeting project using libGDX

---
+++ desktop/src/com/singh/nilesh/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.singh.nilesh.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.singh.nilesh.Supermario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Supermario(), config);
+	}
+}
