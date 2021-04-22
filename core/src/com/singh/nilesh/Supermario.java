e182ae94ba9f4c0114beae4ccbe80506db2e8891
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2021-04-23 00:14:06 +0530

    showing score on screen

--- core/src/com/singh/nilesh/Supermario.java
+++ core/src/com/singh/nilesh/Supermario.java
@@ -2,9 +2,13 @@ package com.singh.nilesh;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -20,13 +24,18 @@ public class Supermario extends ApplicationAdapter {
 	float gravity = 0.2f;
 	int pause=0;
 	Random random;
+	Rectangle manRectangle;
+	int score=0;
+	BitmapFont font;
 
 	ArrayList<Integer> coinX=new ArrayList<>();
+	ArrayList<Rectangle> coinRectangle=new ArrayList<>();
 	ArrayList<Integer> coinY=new ArrayList<>();
 	int coincount=0;
 	Texture coin;
 
 	ArrayList<Integer> bombX=new ArrayList<>();
+	ArrayList<Rectangle> bombRectangle=new ArrayList<>();
 	ArrayList<Integer> bombY=new ArrayList<>();
 	int bombcount=0;
 	Texture bomb;
@@ -44,6 +53,10 @@ public class Supermario extends ApplicationAdapter {
 		coin=new Texture("coin.png");
 		bomb=new Texture("bomb.png");
 		random=new Random();
+		manRectangle=new Rectangle();
+		font=new BitmapFont();
+		font.setColor(Color.WHITE);
+		font.getData().setScale(10);
 	}
 
 	public  void  makeCoin()
@@ -65,7 +78,7 @@ public class Supermario extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
-		if(bombcount<100){
+		if(bombcount<250){
 			bombcount++;
 			/*Gdx.app.log("sdsdf",String.valueOf(coincount));*/
 		}else{
@@ -73,9 +86,11 @@ public class Supermario extends ApplicationAdapter {
 			makeBomb();
 			/*Gdx.app.log("sdsdf","haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");*/
 		}
+		bombRectangle.clear();
 		for(int i=0;i<bombX.size();i++){
 			batch.draw(bomb,bombX.get(i),bombY.get(i));
 			bombX.set(i,bombX.get(i)-8);
+			bombRectangle.add(new Rectangle(bombX.get(i),bombY.get(i),bomb.getWidth(),bomb.getHeight()));
 		}
 
 
@@ -88,9 +103,11 @@ public class Supermario extends ApplicationAdapter {
 			makeCoin();
 			/*Gdx.app.log("sdsdf","haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");*/
 		}
+		coinRectangle.clear();
 		for(int i=0;i<coinX.size();i++){
 			batch.draw(coin,coinX.get(i),coinY.get(i));
 			coinX.set(i,coinX.get(i)-4);
+			coinRectangle.add(new Rectangle(coinX.get(i),coinY.get(i),coin.getWidth(),coin.getHeight()));
 		}
 
 
@@ -122,7 +139,26 @@ public class Supermario extends ApplicationAdapter {
 		}
 
 		batch.draw(man[manstate], Gdx.graphics.getWidth()/2-man[manstate].getWidth()/2,manY);
+        manRectangle.set(Gdx.graphics.getWidth()/2-man[manstate].getWidth()/2,manY,man[manstate].getWidth(),man[manstate].getHeight());
 
+        for(int i=0;i<coinRectangle.size();i++){
+        	if(Intersector.overlaps(manRectangle,coinRectangle.get(i))){
+             score++;
+
+             coinRectangle.remove(i);
+             coinX.remove(i);
+             coinY.remove(i);
+             break;
+			}
+		}
+
+		for(int i=0;i<bombRectangle.size();i++){
+			if(Intersector.overlaps(manRectangle,bombRectangle.get(i))){
+
+			}
+		}
+
+		font.draw(batch,String.valueOf(score),100,200);
 		batch.end();
 	}
 	
